4b77ec585627e75063a6fc940dd7ce230c97d0b0
alan.morais <alan.morais@etec.sp.gov.br>
2022-09-17 09:43:51 -0300

    projeto 17/9/22

---
+++ lib/data/MoneyController.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:money_search/data/api.dart';
+import 'package:money_search/model/MoneyModel.dart';
+import 'package:money_search/model/ListModel.dart';
+
+import '../model/ListModel.dart';
+class MoneyController {
+  /// instancia da classe do DIO
+  final api = API();
+  /// metodo que busca as informações
+
+       Future<List<ListModel>> getlist () async {
+    try {
+      /// metodo get da requisição
+      final response = await api.get(
+          "https://630fa24936e6a2a04edec22f.mockapi.io/api/integridade/getList");
+
+      /// validação de resosta correta
+      if(response?.statusCode == 200){
+        return (json.decode(json.encode(response?.data))as List).map((jsonElement)
+        => ListModel.fromJson(jsonElement)).toList();
+      }else{
+        return [];
+      }
+
+    } catch (e, s) {g
+      print(e);
+      print(s);
+      throw (e);
+    }
+}
+  Future<MoneyModel?> getMoney () async {
+    try {
+      /// metodo get da requisição
+      final response = await api.get(
+          "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL");
+
+      /// validação de resosta correta
+      if(response?.statusCode == 200){
+        return MoneyModel.fromJson(json.decode(json.encode(response?.data)));
+      }else{
+        return null;
+      }
+
+    } catch (e, s) {
+      print(e);
+      print(s);
+      throw (e);
+    }
+  }
+}
+  
